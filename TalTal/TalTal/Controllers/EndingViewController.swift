fb7d1170dbe3794ed5f7f2df55f57f4589e090e7
RuyHa <code.ruyha@gmail.com>
2022-08-12 19:26:43 +0900

    [Feat] 엔딩뷰 기초 작업

--- TalTal/TalTal/Controllers/EndingViewController.swift
+++ TalTal/TalTal/Controllers/EndingViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class EndingViewController: UIViewController {
 
+    @IBAction func closeButton(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,3 +20,12 @@ class EndingViewController: UIViewController {
     }
 
 }
+
+/*
+ 이 View의 작업사항은 다음과 같습니다.
+ 1. 타이틀을 추가해주세요. X버튼의 위치도 수정해야 합니다.
+ 2. 앱을 끝까지 사용한 사용자들에게 해주고 싶은 말을 추가해주세요.
+ 3. 2번이 너무 내용이 많아서 짤린다면 스크롤뷰를 이용해보세요.
+ 4. 언제든지 하다가 막히면 질문하세요.
+ 
+ */
