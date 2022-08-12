938bc6481844de1d868f26231f2d12fb76bd8ccb
RuyHa <code.ruyha@gmail.com>
2022-08-12 13:19:24 +0900

    [Feat] EndingView와 스토리보드 생성후 연결

---
+++ TalTal/TalTal/Controllers/EndingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EndingViewController.swift
+//  TalTal
+//
+//  Created by Ruyha on 2022/08/12.
+//
+
+import UIKit
+
+class EndingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
