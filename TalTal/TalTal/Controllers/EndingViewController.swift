9df4bb35ab070866211751412c9a818684ff51e7
Felix <wonyang22@pos.idserve.net>
2022-08-15 21:28:31 +0900

    [Feat] EndingView의 화면 구성

--- TalTal/TalTal/Controllers/EndingViewController.swift
+++ TalTal/TalTal/Controllers/EndingViewController.swift
@@ -8,18 +8,42 @@
 import UIKit
 
 class EndingViewController: UIViewController {
+    let missionAessts = MissionAssets()
+    // MissionAessts()를 사용하기위해 선언
 
-    @IBAction func closeButton(_ sender: Any) {
-        self.dismiss(animated: true)
-    }
+    @IBOutlet weak var endingSubTitle: UILabel!
+    @IBOutlet weak var endingComment: UILabel!
+    @IBOutlet weak var closeButton: UIBarButtonItem!
+    
+
+    //    @IBAction func closeButton(_ sender: Any) {
+//        self.dismiss(animated: true)
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        settingEndingSubTitleTextLabel()
     }
-
+    
 }
+
+
+//MARK: MissionAessts를 가지고 일일미션과 주간미션의 색상를 변경합니다.
+extension EndingViewController {
+    private func settingEndingSubTitleTextLabel() {
+        let endingSubTitleText1 = "일일 미션 00개 "
+        let endingSubTitleText2 = " 주간 미션 00개"
+
+        let endingSubTitleTextLabelString = missionAessts.changeTextColor(fullText: endingSubTitleText1, color: UIColor(hex: "FF8166"), changeWords: ["일일 미션 00개"])
+
+        let endingSubTitleTextLabelStringPart2 = missionAessts.changeTextColor(fullText: endingSubTitleText2, color: UIColor(hex: "6261F8"), changeWords: ["주간 미션 00개"])
+
+        endingSubTitleTextLabelString.append(endingSubTitleTextLabelStringPart2)
+        endingSubTitle.attributedText = endingSubTitleTextLabelString
+    }
+}
+
+
 
 /*
  이 View의 작업사항은 다음과 같습니다.
